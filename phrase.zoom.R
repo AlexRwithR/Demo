e3b0f81b87d46169a4ff4ca04d81440b75b26df8
AlexRwithR <65188735+AlexRwithR@users.noreply.github.com>
2020-05-25 15:55:51 -0500

    Update phrase.zoom.R

--- phrase.zoom.R
+++ phrase.zoom.R
@@ -21,19 +21,6 @@ phrase.zoom <-function(file.name, z =1, v = NULL){
   return(res.osc)
   }
 
- trytry<-phrase.zoom(yw1.mono, z= 4)
 
-oscillo(yw1.mono)
-# phrase.test <-function(file.name, z =1, v = NULL){
-#   res.osc<- vector('list', z)
-#    if(!is.null(v)){
-#     v<- matrix(v,byrow= TRUE, ncol =2)
-#     n <- nrow(v)
-#     for(i in 1:n){
-#       res.osc[[i]] <-oscillo(file.name, from = v[i,1], to = v[i,2], identify = TRUE, nidentify =2)
-#     }
-#    }
-#   return(res.osc)}
-# trytry<-phrase.test(yw1.mono, v = c(0, 5, 9 ,12, 22, 25, 33, 36))
-#  trytry           
+       
  
