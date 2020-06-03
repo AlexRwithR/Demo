c96afc0349e70543ec0803edbf38a13f60efaf0f
AlexRwithR <65188735+AlexRwithR@users.noreply.github.com>
2020-06-03 10:37:00 -0500

    Update phrase.zoom.R

--- phrase.zoom.R
+++ phrase.zoom.R
@@ -1,22 +1,34 @@
-phrase.zoom <-function(file.name, z =1, v = NULL, r= 1){
+phrase.zoom <-function(file.name, z =1, v = NULL, r = NULL){
   res.osc<- vector('list', z)
   envl<- env(file.name, plot =FALSE)
   f <- file.name@samp.rate
+  o<-1
+  e<-2
   if(!is.null(v)){
-    if(r==1){
+    if(is.null(r)){
     v<- matrix(v,byrow= TRUE, ncol =2)
     n <- nrow(v)
     for(i in 1:n){
       res.osc[[i]] <-oscillo(envl, from = v[i,1], to = v[i,2], f= f,
                              identify = TRUE, nidentify =2)
     }}
+    #Block for if there are multiple idenification points on one zoom
     else{
       v<- matrix(v,byrow= TRUE, ncol =2)
       n <- nrow(v)
+      mes <- vector('list', z)
       for(i in 1:n){
-        res.osc[[i]] <-oscillo(envl, from = v[i,1], to = v[i,2], f= f,
-                               identify = TRUE, nidentify =r) 
-        }}
+        mes[[i]] <-oscillo(envl, from = v[i,1], to = v[i,2], f= f,
+                               identify = TRUE, nidentify =r)
+      }
+      for(s in 1:(length(mes[[1]])/4)){
+        
+      res.osc[[s]]<- matrix(data=c(mes[[1]][o,], mes[[1]][e,]), 
+                            byrow= TRUE, ncol = 2)
+      o<-o +2
+      e<-e+2
+      }
+    }
   }
   else{
   x<- 1
@@ -28,6 +40,5 @@ phrase.zoom <-function(file.name, z =1, v = NULL, r= 1){
   }}
   return(res.osc)
   }
-
        
  
